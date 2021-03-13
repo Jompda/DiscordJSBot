901581921643f3d8ade28e1b2fe1370d6fb25b74
TenDRILLL <32621403+TenDRILLL@users.noreply.github.com>
2021-03-13 13:39:25 +0200

    Removed partials as I don't really use em here.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const bot = new Discord.Client({partials: ["MESSAGE", "CHANNEL"]});
+const bot = new Discord.Client();
 const token = require("./token.json").token;
 
 bot.prefix = "!";
